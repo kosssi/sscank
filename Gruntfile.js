189c233faabd12cd39573b15023118168f072f08
Simon Constans <kosssi@gmail.com>
2013-08-23 18:09:00 +0200

    build CSS & minify files

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            dist: ['dist']
+        },
+        recess: {
+            options: {
+                compile: true
+            },
+            bootstrap: {
+                src: ['src/less/sscank.less'],
+                dest: 'dist/css/<%= pkg.name %>.css'
+            },
+            min: {
+                options: {
+                    compress: true
+                },
+                src: ['src/less/sscank.less'],
+                dest: 'dist/css/<%= pkg.name %>.min.css'
+            }
+        }
+    });
+
+    // Load the plugin
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-recess');
+
+    // Default task(s).
+    grunt.registerTask('default', ['clean', 'recess']);
+};
--- README.md
+++ README.md
@@ -8,9 +8,12 @@ I love the concept but i don't like how it's written...
 Todo list :
  - ☑ Copy all KNACSS composent
  - ☑ Create composer.json
+ - ☑ Build less to CSS
+ - ☑ Create minify CSS
+ - ☐ fix CSS errors
  - ☐ Refactoring of all LESS files
  - ☐ Create bower.json
  - ☐ Create component.json
- - ☐ Build less to CSS
- - ☐ Create minify CSS
  - ☐ Add test for CSS files
+ - ☐ Create documentation for website
+ - ☐ Create documentation for dev
