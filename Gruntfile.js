368cc55c64024e6defe56ae0a0a3156361cbae4c
Simon Constans <kosssi@gmail.com>
2013-08-28 13:54:54 +0200

    add sscank with bower

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        lesslint: {
+            src: ['src/**/*.less']
+        },
+        clean: {
+            dist: ['css']
+        },
+        recess: {
+            options: {
+                compile: true
+            },
+            sscank: {
+                src: ['src/less/sscank.less'],
+                dest: 'dist/css/<%= pkg.name %>.css'
+            }
+        }
+    });
+
+    // Load the plugin
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-recess');
+    grunt.loadNpmTasks('grunt-lesslint');
+
+    // Default task(s).
+    grunt.registerTask('default', ['clean', 'recess']);
+};
